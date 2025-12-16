025920183d024e66a106475bfe6db5c449a45dbb
TomofGL <tertiusofg@gmail.com>
2025-12-16 16:25:52 +0800

    modified:   client/app.js

--- client/app.js
+++ client/app.js
@@ -19,16 +19,34 @@ App({
   getUserProfile() {
     // Try to get user info from storage first
     const storedUserInfo = wx.getStorageSync('userInfo');
-    if (storedUserInfo && storedUserInfo.nickname && storedUserInfo.avatarUrl) {
+    if (storedUserInfo && storedUserInfo.nickname) {
       this.globalData.userInfo.nickname = storedUserInfo.nickname;
-      this.globalData.userInfo.avatarUrl = storedUserInfo.avatarUrl;
+      this.globalData.userInfo.avatarUrl = storedUserInfo.avatarUrl || '';
       console.log('Loaded user info from storage:', storedUserInfo);
       return;
     }
 
-    // If not in storage, we'll prompt user to authorize when needed
-    // WeChat requires user interaction to get profile, so we'll do this in pages
-    console.log('No stored user info, will prompt when needed');
+    // If not in storage, try to get from WeChat (requires user authorization)
+    // Note: wx.getUserProfile requires user interaction, so we'll call it from pages when needed
+    // For now, we'll try the deprecated wx.getUserInfo as fallback for default
+    wx.getUserInfo({
+      success: (res) => {
+        const userInfo = res.userInfo;
+        const profile = {
+          nickname: userInfo.nickName || '',
+          avatarUrl: userInfo.avatarUrl || ''
+        };
+
+        // Save to storage
+        wx.setStorageSync('userInfo', profile);
+        this.globalData.userInfo.nickname = profile.nickname;
+        this.globalData.userInfo.avatarUrl = profile.avatarUrl;
+        console.log('Got WeChat user info:', profile);
+      },
+      fail: (err) => {
+        console.log('Failed to get user info, will prompt when needed:', err);
+      }
+    });
   }
 })
 
